eeadecd1295378fd5862c34596327f8345d00ed3
Gunars Frickauss <gunarsf@gmail.com>
2023-02-24 15:00:03 +0200

    Update initbc.sh

--- initbc.sh
+++ initbc.sh
@@ -7,15 +7,13 @@ systemctl enable barcode.service
 systemctl disable firstboot.service
 raspi-config --expand-rootfs > /dev/null
 CURRENT_HOSTNAME=$(cat /proc/sys/kernel/hostname)
-
 if [ -e /sys/class/net/eth0 ]; then
       MAC=$(cat /sys/class/net/eth0/address | tr -d ":")
 else
       MAC=$(cat /sys/class/net/wlan0/address | tr -d ":")
 fi
 NEW_HOSTNAME="rpi-bc1-"${MAC: -5}
-echo $CURRENT_HOSTNAME
-echo $NEW_HOSTNAME
+echo $CURRENT_HOSTNAME to $NEW_HOSTNAME
 sleep 1
 hostnamectl set-hostname ${NEW_HOSTNAME} --static
 /sbin/shutdown -r now
