632b04054cebd1533bb2a5c825ec24685284ba99
Gunars Frickauss <gunarsf@gmail.com>
2023-02-28 10:17:00 +0200

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -4,7 +4,7 @@ systemctl disable bluetooth.service
 systemctl disable hciuart.service
 apt-get update && apt-get -y upgrade
 
-ln /opt/bc/bc.service /lib/systemd/system/bc.service
+ln /opt/bc/barcode.service /lib/systemd/system/barcode.service
 ln /opt/bc/firstboot.service /lib/systemd/system/firstboot.service
 ln /opt/bc/bc.ini /home/pi/bc.ini
 systemctl enable firstboot.service
